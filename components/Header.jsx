ec5784cfb946fc9f90a0c67b84b01e1a5b085342
abhinav <abhinavdhama@gmail.com>
2025-04-03 17:46:22 +0530

    initial commit

---
+++ components/Header.jsx
@@ -0,0 +1,83 @@
+import Image from "next/image";
+import React from "react";
+import { Button } from "./ui/button";
+import Link from "next/link";
+import { ArrowLeft, CarFront, Heart, Layout } from "lucide-react";
+import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
+
+export const Header = ({ isAdminPage = false }) => {
+  const isAdmin = false;
+  return (
+    <header className="fixed top-0 w-full bg-white/80 backdrop-blur-md z-50 border-b">
+      <nav className="mx-auto px-4 py-4 flex items-center justify-between">
+        <Link className="flex" href={isAdminPage ? "/admin" : "/"}>
+          <Image
+            src={"/logo-black.png"}
+            alt="Vehiql Logo"
+            width={200}
+            height={60}
+            className="h-12 w-auto object-contain"
+          />
+          {isAdminPage && (
+            <span className="text-xs font-extralight">admin</span>
+          )}
+        </Link>
+        {isAdminPage ? (
+          <>
+            <Link href="/">
+              <Button variant="outline" className="flex items-center gap-2">
+                <ArrowLeft size={18} />
+                <span>Back to App</span>
+              </Button>
+            </Link>
+          </>
+        ) : (
+          <SignedIn>
+            {!isAdmin && (
+              <Link
+                href="/reservations"
+                className="text-gray-600 hover:text-blue-600 flex items-center gap-2"
+              >
+                <Button variant="outline">
+                  <CarFront size={18} />
+                  <span className="hidden md:inline">My Reservations</span>
+                </Button>
+              </Link>
+            )}
+            <a href="/saved-cars">
+              <Button className="flex items-center gap-2">
+                <Heart size={18} />
+                <span className="hidden md:inline">Saved Cars</span>
+              </Button>
+            </a>
+            {isAdmin && (
+              <Link href="/admin">
+                <Button variant="outline" className="flex items-center gap-2">
+                  <Layout size={18} />
+                  <span className="hidden md:inline">Admin Portal</span>
+                </Button>
+              </Link>
+            )}
+          </SignedIn>
+        )}
+        <SignedOut>
+          {!isAdminPage && (
+            <SignInButton forceRedirectUrl="/">
+              <Button variant="outline">Login</Button>
+            </SignInButton>
+          )}
+        </SignedOut>
+
+        <SignedIn>
+          <UserButton
+            appearance={{
+              elements: {
+                avatarBox: "w-10 h-10",
+              },
+            }}
+          />
+        </SignedIn>
+      </nav>
+    </header>
+  );
+};
